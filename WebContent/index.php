58be44c410eb684152c11396fa1e8944d347da4d
Mårten Dahlquist <marten@dahlquist.info>
2017-02-25 19:46:46 +0100

    Lista filer

--- WebContent/index.php
+++ WebContent/index.php
@@ -6,12 +6,24 @@
 		</script>
 	<head>
 	<body>
-	<?php
+<?php
 	$dir = "../../../shares/nedladdat";
 	if (is_dir($dir)){
+		$cnt = 0;
   		if ($dh = opendir($dir)){
     	while (($file = readdir($dh)) !== false){
-      		echo "filename:" . $file . "<br>";
+			if (strpos($file, '.') !== false){ 
+				$fileFix = substr($file,0,-4);
+				if(strlen($fileFix)!=0 && $cnt < 10){
+					$json=file_get_contents("http://www.omdbapi.com/?t=$fileFix");
+					$info=json_decode($json);
+?>
+					<img src="<?php echo $info->Poster ?>" height="300" width="200"/><br/>
+					<?php echo $info->Title ?>
+<?php					
+					$cnt++;
+				}
+			}
     	}
     	closedir($dh);
   	}
