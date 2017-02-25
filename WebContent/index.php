1d8ca66c249946f011c877ab11ec535971ae88f6
Mårten Dahlquist <marten@dahlquist.info>
2017-02-25 21:01:51 +0100

    Lite styling

--- WebContent/index.php
+++ WebContent/index.php
@@ -1,6 +1,7 @@
 <html>
 	<head>
 		<link rel="shortcut icon" href="images/mainicon.ico">
+		<link rel="stylesheet" href="style/main.css">
 		<title>Movie Database</title>
 		<script type="text/javascript">
 		</script>
@@ -11,17 +12,28 @@
 	if (is_dir($dir)){
 		$cnt = 0;
   		if ($dh = opendir($dir)){
-    	while (($file = readdir($dh)) !== false){
+    	while (($file = readdir($dh)) !== false && $cnt < 20){
 			if (strpos($file, '.') !== false){ 
 				$fileFix = substr($file,0,-4);
-				if(strlen($fileFix)!=0 && $cnt < 10){
-					$json=file_get_contents("http://www.omdbapi.com/?t=$fileFix");
+				if(strlen($fileFix)!=0){
+					$fileEncode = urlencode($fileFix);
+					$json=file_get_contents("http://www.omdbapi.com/?t=$fileEncode");
 					$info=json_decode($json);
+					if(strlen($info->Title)!=0){
 ?>
+					<p class="title"><?php echo $info->Title ?></p><br/>
+					<p class="main"><?php echo $info->Runtime ?><br/>
+					<?php echo $info->Plot ?><br/>
+					<?php echo $info->Actors ?><br/>
+					<?php echo $info->imdbRating ?><br/></p>
+					<a href="http://www.imdb.com/title/<?php echo $info->imdbID ?>" target="_blank">IMDB</a><br/>
 					<img src="<?php echo $info->Poster ?>" height="300" width="200"/><br/>
-					<?php echo $info->Title ?>
 <?php					
 					$cnt++;
+					}
+					else{
+						echo "<p class='main'>Hittade ingen info för ".$fileFix."</p><br>";
+					}
 				}
 			}
     	}
